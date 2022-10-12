2e600149dca79b883eb2e31fe3608164cbd469af
LizaLasht <112845770+LizaLasht@users.noreply.github.com>
2022-10-12 19:53:23 +0300

    Update index5.js

--- index5.js
+++ index5.js
@@ -2,7 +2,7 @@ function click()
 {
     var result;
     let f1=document.getElementsById('field1').value;
-    let f2 =document.getElementsById('field2').value;
+    let f2=document.getElementsById('field2').value;
     let r1=document.getElementById('result');
     const price=/^-?(0|[1-9]\d*)(\.[0-9]{1,4})?$/;
     const k=/^(0|-?[1-9]\d{0,5})$/;
@@ -11,7 +11,7 @@ function click()
     else if( !price.test(f1))  r1.innerHTML = "Введено неверное количество товара";
     else if(!k.test(f2))  r1.innerHTML = "Введена неверная цена ";
     else
-    { var result=f1*f2;
+    { result=f1*f2;
         r1.innerHTML ="Стоимость заказа: "+ result;
     }
     return false;
