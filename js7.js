5efa7d870d049dac7ed2569d6cb9c7df57648c35
LizaLasht <112845770+LizaLasht@users.noreply.github.com>
2022-10-23 01:23:32 +0300

    Update js7.js

--- js7.js
+++ js7.js
@@ -1,8 +1,17 @@
-$(function(){
-    $('.rev_slider').slick({  
+ $(document).ready(function () {
+     $('.rev_slider').slick({  
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 4,
-    dots:true
-    })
-})
+    dots:true,
+     responsive: [
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+  ]
+  });
+});
